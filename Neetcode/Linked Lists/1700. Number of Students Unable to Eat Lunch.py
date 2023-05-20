f75420dc14720bbdd44275dc545b1d3683ce50cf
Arkin Mukherjee <arkinmukherjee@gmail.com>
2023-05-19 20:56:45 -0400

    Add faster solution for linked list problem 5

--- Neetcode/Linked Lists/1700. Number of Students Unable to Eat Lunch.py
+++ Neetcode/Linked Lists/1700. Number of Students Unable to Eat Lunch.py
@@ -11,3 +11,26 @@ class Solution:
                 students.append(student)
 
         return len(students)
+
+    def countStudents2(self, students: List[int], sandwiches: List[int]) -> int:
+        zeros, ones = 0, 0
+
+        # count numbers of student preferences
+        for student in students:
+            if student == 0:
+                zeros += 1
+            else:
+                ones += 1
+
+        for sandwich in sandwiches:
+            if sandwich == 0:  # if no one wants sandwich 0
+                if zeros == 0:
+                    return ones
+                zeros -= 1
+            else:
+                if ones == 0:  # if no one wants sandwich 1
+                    return zeros
+                ones -= 1
+
+        return 0
+
