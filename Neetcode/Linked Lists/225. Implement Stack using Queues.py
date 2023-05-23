54f9951a4677ae642ae85a6f07df97da9d916cae
Arkin Mukherjee <arkinmukherjee@Arkins-MacBook-Pro.local>
2023-05-23 02:10:32 -0400

    Add solution for linked list problem 6

---
+++ Neetcode/Linked Lists/225. Implement Stack using Queues.py
@@ -0,0 +1,30 @@
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        length = len(self.queue)
+        val = 0
+        while length > 1:
+            val = self.queue.popleft()
+            self.queue.append(val)
+            length -= 1
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[-1]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
